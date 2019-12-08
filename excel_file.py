1c79a4e2cba9f3127974bd6e1ab8527e49e3714d
Romiss <Romissevd@gmail.com>
2019-12-08 21:58:59 +0200

    match table

--- excel_file.py
+++ excel_file.py
@@ -3,26 +3,32 @@ import xlsxwriter
 from bot import get_username
 from db_data_users import MongoDB
 
+# FILE_NAME = datetime.datetime.strftime('%Y_%m_%d_%H_%m_%s')
 FILE_NAME = 'test'
 db = MongoDB()
-workbook = xlsxwriter.Workbook(FILE_NAME + '.xlsx')
-worksheet = workbook.add_worksheet('test')
 
-worksheet.write(0, 0, 'users')
 
-row = 1
-col = 1
-col_matches = 0
-for user in db.get_all_bets():
-    res_row = 1
-    all_matches_tour = db.get_all_matches_tour(user['id_telegram'])
-    for match in all_matches_tour:
-        if row < len(all_matches_tour) + 1:
-            worksheet.write(row, col_matches, match['match'])
-            row += 1
-        worksheet.write(res_row, col, match['result'])
-        res_row += 1
-    worksheet.write(0, col, get_username(user['user']))
-    col += 1
+def write_result_matches_in_excel():
+    workbook = xlsxwriter.Workbook(FILE_NAME + '.xlsx')
+    worksheet = workbook.add_worksheet('test')
+    worksheet.write(0, 0, 'users')
+    row = 1
+    col = 1
+    col_matches = 0
+    for user in db.get_all_bets():
+        res_row = 1
+        all_matches_tour = db.get_all_matches_tour(user['id_telegram'])
+        for match in all_matches_tour:
+            if row < len(all_matches_tour) + 1:
+                worksheet.write(row, col_matches, match['match'])
+                row += 1
+            worksheet.write(res_row, col, match['result'])
+            res_row += 1
+        worksheet.write(0, col, get_username(user['user']))
+        col += 1
+    workbook.close()
 
-workbook.close()
+
+write_result_matches_in_excel()
+
+# workbook.close()
